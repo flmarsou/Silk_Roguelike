873eb28efe9e2105dc67f95ac242d32527bbaf54
flmarsou <marsouinflorent12@gmail.com>
2025-06-11 00:32:29 +0200

    Added main (debug)

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿class	Program
+{
+	private static void	Main()
+	{
+		Tile[,]	map = Dungeon.GenerateDungeon();
+
+		for (int y = 0; y < map.GetLength(0); y++)
+		{
+			for (int x = 0; x < map.GetLength(1); x++)
+			{
+				switch (map[y, x])
+				{
+					case (Tile.Empty):
+						Console.Write("  ");
+						break ;
+					case (Tile.Wall):
+						Console.Write("# ");
+						break ;
+					case (Tile.Floor):
+						Console.Write(". ");
+						break ;
+					default:
+						Console.Write("? ");
+						break ;
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+}
