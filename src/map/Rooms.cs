5feef6b755106506a56dd36f034ee96dfa807d3c
flmarsou <marsouinflorent12@gmail.com>
2025-06-11 00:32:47 +0200

    Added room object

---
+++ src/map/Rooms.cs
@@ -0,0 +1,39 @@
+public partial class	Dungeon
+{
+	private class	Room(int x, int y, int width, int height)
+	{
+		public int	X { get; } = x;
+		public int	Y { get; } = y;
+		public int	Width { get; } = x + width;
+		public int	Height { get; } = y + height;
+
+		public bool	Overlap(Room room, int padding)
+		{
+			return !(this.Width + padding <= room.X
+					|| this.X - padding >= room.Width
+					|| this.Height + padding <= room.Y
+					|| this.Y - padding >= room.Height);
+		}
+
+		public (int x, int y)	Center()
+		{
+			return ((X + Width) / 2, (Y + Height) / 2);
+		}
+	}
+
+	private static void	AddRoom(Tile[,] map, Room room)
+	{
+		for (int y = room.Y; y < room.Height; y++)
+		{
+			for (int x = room.X; x < room.Width; x++)
+			{
+				// Place [Wall Tiles] on the edges of the room
+				if (y == room.Y || y == room.Height - 1 || x == room.X || x == room.Width - 1)
+					map[y, x] = Tile.Wall;
+				// Place [Floor Tiles] on the inside of the room
+				else
+					map[y, x] = Tile.Floor;
+			}
+		}
+	}
+}
