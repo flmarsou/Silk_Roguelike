eea07a572926b9ca12815b3627b8463b9286623c
flmarsou <marsouinflorent12@gmail.com>
2025-06-16 22:00:45 +0200

    Create Player.cs

---
+++ src/Player.cs
@@ -0,0 +1,17 @@
+class	Player(int x, int y)
+{
+	public int	X { get; set; } = x;
+	public int	Y { get; set; } = y;
+
+	public void	Move(int dx, int dy, Tile[,] map)
+	{
+		int	newX = X + dx;
+		int	newY = Y + dy;
+
+		if (map[newY, newX] != Tile.Wall)
+		{
+			X = newX;
+			Y = newY;
+		}
+	}
+}
