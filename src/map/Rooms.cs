9b0576f07a9b4473b24a8d87b7e9b2fc4720555f
flmarsou <marsouinflorent12@gmail.com>
2025-06-17 13:02:30 +0200

    Added comments and swapped X with Y for clarity

--- src/map/Rooms.cs
+++ src/map/Rooms.cs
@@ -1,28 +1,43 @@
 public partial class	Dungeon
 {
-	private class	Room(int x, int y, int width, int height)
+	private class	Room(int y, int x, int width, int height)
 	{
-		public int	X { get; } = x;
 		public int	Y { get; } = y;
-		public int	Width { get; } = x + width;
+		public int	X { get; } = x;
 		public int	Height { get; } = y + height;
+		public int	Width { get; } = x + width;
 
-		public bool	Overlap(Room room, int padding)	=>	!(this.Width + padding <= room.X
-														|| this.X - padding >= room.Width
-														|| this.Height + padding <= room.Y
-														|| this.Y - padding >= room.Height);
+		/// <summary>
+		/// Returns true if the context room is within the boundries of the param room. <br/>
+		/// Returns false otherwise.
+		/// </summary>
+		public bool	Overlap(Room room, int padding)	=>	!(this.Height + padding <= room.Y
+														|| this.Y - padding >= room.Height
+														|| this.Width + padding <= room.X
+														|| this.X - padding >= room.Width);
 
-		public bool	IsInRoom(int x, int y) 			=>	(x >= this.X
-														&& x < this.Width
-														&& y >= this.Y
-														&& y < this.Height);
+		/// <summary>
+		/// Returns true if the (y, x) coordinates are inside the context room, including its walls. <br/>
+		/// Returns false otherwise.
+		/// </summary>
+		public bool	IsInRoom(int y, int x)	=>	(y >= this.Y
+												&& y < this.Height
+												&& x >= this.X
+												&& x < this.Width);
 
-		public bool	IsInsideRoom(int x, int y)		=>	(x >= this.X + 1
-														&& x < this.Width - 1
-														&& y >= this.Y + 1
-														&& y < this.Height - 1);
+		/// <summary>
+		/// Returns true if the (y, x) coordinates are inside the context room, excluding its walls. <br/>
+		/// Returns false otherwise.
+		/// </summary>
+		public bool	IsInsideRoom(int y, int x)	=>	(y >= this.Y + 1
+													&& y < this.Height - 1
+													&& x >= this.X + 1
+													&& x < this.Width - 1);
 
-		public (int y, int x)	Center =>	((Y + Height) / 2, (X + Width) / 2);
+		/// <summary>
+		/// Returns the approximate center (y, x) coordinates of the context room.
+		/// </summary>
+		public (int y, int x)	Center	=>	((Y + Height) / 2, (X + Width) / 2);
 	}
 
 	/// <summary>
@@ -47,10 +62,10 @@ public partial class	Dungeon
 	/// <summary>
 	/// Checks if the (x, y) coordinates are inside any room, including its walls.
 	/// </summary>
-	private static bool	IsInAnyRoom(List<Room> rooms, int x, int y)
+	private static bool	IsInAnyRoom(List<Room> rooms, int y, int x)
 	{
 		foreach (Room room in rooms)
-			if (room.IsInRoom(x, y))
+			if (room.IsInRoom(y, x))
 				return (true);
 		return (false);
 	}
@@ -58,10 +73,10 @@ public partial class	Dungeon
 	/// <summary>
 	/// Checks if the (x, y) coordinates are inside any room, excluding its walls.
 	/// </summary>
-	private static bool	IsInsideAnyRoom(List<Room> rooms, int x, int y)
+	private static bool	IsInsideAnyRoom(List<Room> rooms, int y, int x)
 	{
 		foreach (Room room in rooms)
-			if (room.IsInsideRoom(x, y))
+			if (room.IsInsideRoom(y, x))
 				return (true);
 		return (false);
 	}
--- src/map/Tunnels.cs
+++ src/map/Tunnels.cs
@@ -2,43 +2,22 @@ public partial class	Dungeon
 {
 	private static void	DigTunnel(Tile[,] map, (int y, int x)from, (int y, int x)to, List<Room> rooms)
 	{
-		int		x = from.x;
 		int		y = from.y;
-		bool	shouldPlaceDoor;
+		int		x = from.x;
 		bool	hasPlacedDoor = false;
 
-		while (x != to.x)
-		{
-			if (IsInsideAnyRoom(rooms, x, y))
-			{
-				x += Math.Sign(to.x - x);
-				continue ;
-			}
-
-			shouldPlaceDoor = IsCorrectPlacement(map, x, y);
-
-			if (!hasPlacedDoor && shouldPlaceDoor && _rng.Next(3) == 0)
-			{
-				map[y, x] = Tile.Door;
-				hasPlacedDoor = true;
-			}
-			else
-				map[y, x] = Tile.Tunnel;
-
-			x += Math.Sign(to.x - x);
-		}
-
+		// Dig vertically
 		while (y != to.y)
 		{
-			if (IsInsideAnyRoom(rooms, x, y))
+			// Skip digging if inside a room
+			if (IsInsideAnyRoom(rooms, y, x))
 			{
 				y += Math.Sign(to.y - y);
 				continue ;
 			}
 
-			shouldPlaceDoor = IsCorrectPlacement(map, x, y);
-
-			if (!hasPlacedDoor && shouldPlaceDoor && _rng.Next(3) == 0)
+			// With 25% chance, place a door if not already placed, and if the position is valid
+			if (!hasPlacedDoor && IsCorrectPlacement(map, y, x) && _rng.Next(3) == 0)
 			{
 				map[y, x] = Tile.Door;
 				hasPlacedDoor = true;
@@ -48,14 +27,36 @@ public partial class	Dungeon
 
 			y += Math.Sign(to.y - y);
 		}
+
+		// Dig horizontally
+		while (x != to.x)
+		{
+			// Skip digging if inside a room
+			if (IsInsideAnyRoom(rooms, y, x))
+			{
+				x += Math.Sign(to.x - x);
+				continue ;
+			}
+
+			// With 25% chance, place a door if not already placed, and if the position is valid
+			if (!hasPlacedDoor && IsCorrectPlacement(map, y, x) && _rng.Next(3) == 0)
+			{
+				map[y, x] = Tile.Door;
+				hasPlacedDoor = true;
+			}
+			else
+				map[y, x] = Tile.Tunnel;
+
+			x += Math.Sign(to.x - x);
+		}
 	}
 
 	/// <summary>
-	/// Checks if there are `Empty Tiles` above and below OR right and left.
+	/// Returns true if the given tile has `Empty Tiles` above and below OR left and right.
 	/// </summary>
-	private static bool	IsCorrectPlacement(Tile[,] map, int x, int y)
+	private static bool	IsCorrectPlacement(Tile[,] map, int y, int x)
 	{
 		return ((map[y - 1, x] == Tile.Empty && map[y + 1, x] == Tile.Empty)
-				|| (map[y, x + 1] == Tile.Empty && map[y, x - 1] == Tile.Empty));
+				|| (map[y, x - 1] == Tile.Empty && map[y, x + 1] == Tile.Empty));
 	}
 }
