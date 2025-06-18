f5f16ff4125b40f5ac4ecfe6c9145f8839eb872f
flmarsou <marsouinflorent12@gmail.com>
2025-06-18 21:59:24 +0200

    Temporally commented inputs & OnUpdate

--- src/OnUpdate.cs
+++ src/OnUpdate.cs
@@ -1,16 +1,16 @@
 public partial class	Program
 {
-	private static float	_accumulator = 0.0f;
-	private const float		_updateInterval = 0.5f;
+	// private static float	_accumulator = 0.0f;
+	// private const float		_updateInterval = 0.5f;
 
 	private static void	OnUpdate(double deltaTime)
 	{
-		_accumulator += (float)deltaTime;
+		// _accumulator += (float)deltaTime;
 
-		if (_accumulator >= _updateInterval)
-		{
-			_enemy.Act(_map, _player.Y, _player.X);
-			_accumulator = 0.0f;
-		}
+		// if (_accumulator >= _updateInterval)
+		// {
+		// 	_enemy.Act(_map, _player.Y, _player.X);
+		// 	_accumulator = 0.0f;
+		// }
 	}
 }
