34adbd24147ec82a6dd7f8593cac859b50fa5e6b
flmarsou <marsouinflorent12@gmail.com>
2025-06-18 22:00:14 +0200

    Temporally commented CMD debugger for quad drawings

--- src/OnRender.cs
+++ src/OnRender.cs
@@ -1,55 +1,65 @@
+using Silk.NET.OpenGL;
+
 public partial class	Program
 {
-	private static void	OnRender(double deltaTime)
+	private static unsafe void	OnRender(double deltaTime)
 	{
-		DisplayMap();
+		// DisplayMap();
+		_gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+
+		// Bind the VAO and Shader Program
+		_gl.BindVertexArray(_vao);
+		_gl.UseProgram(_program);
+
+		// Draw the quad
+		_gl.DrawElements(PrimitiveType.Triangles, (uint)_indices.Length, DrawElementsType.UnsignedInt, null);
 	}
 
-	private static void	DisplayMap()
-	{
-		Console.Clear();
+	// private static void	DisplayMap()
+	// {
+	// 	Console.Clear();
 
-		for (int y = 0; y < _map.GetLength(0); y++)
-		{
-			for (int x = 0; x < _map.GetLength(1); x++)
-			{
-				// Player
-				if (_player.Y == y && _player.X == x)
-				{
-					if (_player.Health >= 10)
-						Console.ForegroundColor = ConsoleColor.Green;
-					else if (_player.Health > 5)
-						Console.ForegroundColor = ConsoleColor.Yellow;
-					else
-						Console.ForegroundColor = ConsoleColor.Red;
-					Console.Write("P ");
+	// 	for (int y = 0; y < _map.GetLength(0); y++)
+	// 	{
+	// 		for (int x = 0; x < _map.GetLength(1); x++)
+	// 		{
+	// 			// Player
+	// 			if (_player.Y == y && _player.X == x)
+	// 			{
+	// 				if (_player.Health >= 10)
+	// 					Console.ForegroundColor = ConsoleColor.Green;
+	// 				else if (_player.Health > 5)
+	// 					Console.ForegroundColor = ConsoleColor.Yellow;
+	// 				else
+	// 					Console.ForegroundColor = ConsoleColor.Red;
+	// 				Console.Write("P ");
 
-					Console.ResetColor();
-					continue ;
-				}
+	// 				Console.ResetColor();
+	// 				continue ;
+	// 			}
 
-				// Enemy
-				if (_enemy.Y == y && _enemy.X == x)
-				{
-					Console.Write("E ");
-					continue ;
-				}
+	// 			// Enemy
+	// 			if (_enemy.Y == y && _enemy.X == x)
+	// 			{
+	// 				Console.Write("E ");
+	// 				continue ;
+	// 			}
 
-				// Tiles
-				switch (_map[y, x])
-				{
-					case (Tile.Empty): Console.Write("  "); break ;
-					case (Tile.Wall): Console.Write("# "); break ;
-					case (Tile.Floor): Console.Write(". "); break ;
-					case (Tile.Tunnel): Console.Write(". "); break ;
-					case (Tile.Door): Console.Write("D "); break ;
-					case (Tile.DoorOpen): Console.Write("d "); break ;
-					default: Console.Write("? "); break ;
-				}
-			}
-			Console.WriteLine();
-		}
+	// 			// Tiles
+	// 			switch (_map[y, x])
+	// 			{
+	// 				case (Tile.Empty): Console.Write("  "); break ;
+	// 				case (Tile.Wall): Console.Write("# "); break ;
+	// 				case (Tile.Floor): Console.Write(". "); break ;
+	// 				case (Tile.Tunnel): Console.Write(". "); break ;
+	// 				case (Tile.Door): Console.Write("D "); break ;
+	// 				case (Tile.DoorOpen): Console.Write("d "); break ;
+	// 				default: Console.Write("? "); break ;
+	// 			}
+	// 		}
+	// 		Console.WriteLine();
+	// 	}
 
-		Console.WriteLine("Player HP: " + _player.Health);
-	}
+	// 	Console.WriteLine("Player HP: " + _player.Health);
+	// }
 }
