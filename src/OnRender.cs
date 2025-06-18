6c2e83dfd84b931c459f1a86d5d119d7803cc122
flmarsou <marsouinflorent12@gmail.com>
2025-06-18 16:15:14 +0200

    Added player health display

--- src/OnRender.cs
+++ src/OnRender.cs
@@ -13,35 +13,43 @@ public partial class	Program
 		{
 			for (int x = 0; x < _map.GetLength(1); x++)
 			{
-				if (_player.X == x && _player.Y == y)
+				// Player
+				if (_player.Y == y && _player.X == x)
 				{
+					if (_player.Health >= 10)
+						Console.ForegroundColor = ConsoleColor.Green;
+					else if (_player.Health > 5)
+						Console.ForegroundColor = ConsoleColor.Yellow;
+					else
+						Console.ForegroundColor = ConsoleColor.Red;
 					Console.Write("P ");
+
+					Console.ResetColor();
 					continue ;
 				}
 
+				// Enemy
+				if (_enemy.Y == y && _enemy.X == x)
+				{
+					Console.Write("E ");
+					continue ;
+				}
+
+				// Tiles
 				switch (_map[y, x])
 				{
-					case (Tile.Empty):
-						Console.Write("  ");
-						break ;
-					case (Tile.Wall):
-						Console.Write("# ");
-						break ;
-					case (Tile.Floor):
-						Console.Write(". ");
-						break ;
-					case (Tile.Tunnel):
-						Console.Write(". ");
-						break ;
-					case (Tile.Door):
-						Console.Write("D ");
-						break ;
-					default:
-						Console.Write("? ");
-						break ;
+					case (Tile.Empty): Console.Write("  "); break ;
+					case (Tile.Wall): Console.Write("# "); break ;
+					case (Tile.Floor): Console.Write(". "); break ;
+					case (Tile.Tunnel): Console.Write(". "); break ;
+					case (Tile.Door): Console.Write("D "); break ;
+					case (Tile.DoorOpen): Console.Write("d "); break ;
+					default: Console.Write("? "); break ;
 				}
 			}
 			Console.WriteLine();
 		}
+
+		Console.WriteLine("Player HP: " + _player.Health);
 	}
 }
