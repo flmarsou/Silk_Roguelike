58817aa2583d3a5e88e1c5d71f61d4f5149d64d4
flmarsou <marsouinflorent12@gmail.com>
2025-06-18 16:14:23 +0200

    Added door mechanic

--- src/entities/Player.cs
+++ src/entities/Player.cs
@@ -1,17 +1,26 @@
-class	Player(int x, int y)
+class	Player(int y, int x)
 {
-	public int	X { get; set; } = x;
 	public int	Y { get; set; } = y;
+	public int	X { get; set; } = x;
+	public int	Health { get; set; } = 10;
 
-	public void	Move(int dx, int dy, Tile[,] map)
+	public void	Move(int dy, int dx, Tile[,] map)
 	{
-		int	newX = X + dx;
-		int	newY = Y + dy;
+		int	newY = this.Y + dy;
+		int	newX = this.X + dx;
+
+		if (map[newY, newX] == Tile.Door)
+		{
+			map[newY, newX] = Tile.DoorOpen;
+			return ;
+		}
 
 		if (map[newY, newX] != Tile.Wall)
 		{
-			X = newX;
-			Y = newY;
+			this.Y = newY;
+			this.X = newX;
 		}
 	}
+
+	public void	Hurt(int hitPoint)	=>	this.Health -= hitPoint;
 }
