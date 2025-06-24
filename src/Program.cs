4a0ce450de505a6c9c0eae63218ede14ed0161b2
flmarsou <marsouinflorent12@gmail.com>
2025-06-24 15:42:25 +0200

    Added window size variables

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,8 @@ using Silk.NET.Windowing;
 public partial class	Program
 {
 	private static IWindow	_window;
+	private static int		_windowWidth;
+	private static int		_windowHeight;
 
 	private static void	Main()
 	{
@@ -26,6 +28,8 @@ public partial class	Program
 
 	private static void	OnResize(Vector2D<int> newSize)
 	{
+		_windowWidth = _window.FramebufferSize.X;
+		_windowHeight = _window.FramebufferSize.Y;
 		_gl.Viewport(newSize);
 	}
 
