4bae156077888fa6178e4ecb920aebbb6edb6b41
flmarsou <marsouinflorent12@gmail.com>
2025-06-24 15:42:47 +0200

    Minor changes

--- src/OnUpdate.cs
+++ src/OnUpdate.cs
@@ -1,16 +1,14 @@
 public partial class	Program
 {
-	// private static float	_accumulator = 0.0f;
-	// private const float		_updateInterval = 0.5f;
+	private static Tile[,]		_map = null;
+	private static TextureID[,]	_texMap = null;
 
 	private static void	OnUpdate(double deltaTime)
 	{
-		// _accumulator += (float)deltaTime;
-
-		// if (_accumulator >= _updateInterval)
-		// {
-		// 	_enemy.Act(_map, _player.Y, _player.X);
-		// 	_accumulator = 0.0f;
-		// }
+		if (_map == null)
+		{
+			_map = Dungeon.GenerateDungeon();
+			_texMap = Dungeon.GenerateTextureMap(_map);
+		}
 	}
 }
