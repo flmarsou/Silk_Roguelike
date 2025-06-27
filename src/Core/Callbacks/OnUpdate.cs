36510f1c9113f83786dab4a4148af75c555d41ba
flmarsou <marsouinflorent12@gmail.com>
2025-06-27 15:05:30 +0200

    Added player and enemy update

--- src/Core/Callbacks/OnUpdate.cs
+++ src/Core/Callbacks/OnUpdate.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 public partial class	Program
 {
 	private static Button	_playButton;
@@ -7,6 +9,12 @@ public partial class	Program
 	private static Tile[,]		_map = null;
 	private static TextureID[,]	_texMap = null;
 
+	private static Player		_player;
+
+	private static double		_enemyActionTimer = 0.0;
+	private static double		_enemyActionInterval = 1.0;
+	private static Enemy		_enemy;
+
 	// ====================================================================== //
 	//                            Main Update Loop                            //
 	// ====================================================================== //
@@ -15,7 +23,7 @@ public partial class	Program
 		switch (_state)
 		{
 			case (GameState.MainMenu): UpdateMenu(); return ;
-			case (GameState.Game): UpdateGame(); return ;
+			case (GameState.Game): UpdateGame(deltaTime); return ;
 		}
 	}
 
@@ -32,12 +40,50 @@ public partial class	Program
 	// ====================================================================== //
 	//                             Game Updating                              //
 	// ====================================================================== //
-	private static void	UpdateGame()
+	private static void	UpdateGame(double deltaTime)
 	{
 		if (_map == null)
 		{
 			_map = Dungeon.GenerateDungeon();
 			_texMap = Dungeon.GenerateTextureMap(_map);
+			(int y, int x) = FindPlayerPosition();
+			_player = new Player(y, x);
+			(int y2, int x2) = FindEnemyPosition();
+			_enemy = new Enemy(y2, x2);
 		}
+
+		_enemyActionTimer += deltaTime;
+
+		if (_enemyActionTimer >= _enemyActionInterval)
+		{
+			_enemy.Act(_map, _player.Y, _player.X);
+			_enemyActionTimer = 0.0;
+		}
+	}
+
+	private static (int y, int x)	FindPlayerPosition()
+	{
+		int	height = _map.GetLength(0);
+		int	width = _map.GetLength(1);
+
+		for (int y = 0; y < height; y++)
+			for (int x = 0; x < width; x++)
+				if (_map[y, x] == Tile.PlayerSpawn)
+					return ((y, x));
+
+		return (-1, -1);
+	}
+
+	private static (int y, int x)	FindEnemyPosition()
+	{
+		int	height = _map.GetLength(0);
+		int	width = _map.GetLength(1);
+
+		for (int y = 0; y < height; y++)
+			for (int x = 0; x < width; x++)
+				if (_map[y, x] == Tile.EnemySpawn)
+					return ((y, x));
+
+		return (-1, -1);
 	}
 }
